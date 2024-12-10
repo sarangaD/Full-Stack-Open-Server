ad162663723457e458a63e6c09f3b5b121a33da3
sarangaD <saranga.ucsc@gmail.com>
2024-12-10 10:07:24 +0200

    Commit 3-1

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(morgan('tiny'))
-app.use(express.static('dist'))
 
 let persons = [
     {
@@ -29,23 +23,7 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-const date = new Date();
 
-
-morgan.token('post-data', (req) => {
-    return req.method === 'POST' ? JSON.stringify(req.body) : '';
-  });
-  const customFormat = ':method :url :status :response-time ms - :post-data';
-
-  app.use(
-    morgan(customFormat, {
-      skip: (req) => req.method !== 'POST', // Log only POST requests
-    })
-  );
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
 
 app.get('/api/info', (request, response) => {
     response.send(`<h3>Phonebook has info for ${persons.length} people </h3> <br>
@@ -58,55 +36,9 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
-
-app.post('/api/persons', (request, response) => {
-    const body = request.body;
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    if (persons.filter(x => x.name === body.name).length > 0) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: getRandomInt(10000),
-    }
-    persons.concat(person);
-    response.json(person)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
-app.use(unknownEndpoint)
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
 
 
